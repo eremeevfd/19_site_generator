9253b522c6e625249f1cd2dadea19932f7f67b49
eremeevfd <dartfedor@mail.ru>
2017-01-22 09:26:04 +0300

    Moved logic to functions.

--- static_site_generator.py
+++ static_site_generator.py
@@ -14,45 +14,63 @@ def load_json_config():
     return articles_list
 
 
-def convert_articles_to_html(articles):
-    for article in articles:
-        html_article_path = 'site/articles/{0}.html'.format(os.path.splitext(article['source'])[0])
-        article['html'] = html_article_path
-        logger.info(os.path.dirname(html_article_path))
+def markdown_filter(text):
+    md = markdown.Markdown(extensions=['meta'])
+    return Markup(md.convert(text))
+
+
+def create_jinja_environment():
+    env = Environment(loader=FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
+    env.filters['markdown'] = markdown_filter
+    return env
+
+
+def open_markdown_article_from_file(md_article):
+    with open('articles/{0}'.format(md_article['source'])) as article_file:
+        article_file = article_file.read()
+        return article_file
+
+
+def convert_markdown_to_html(article_file):
+    html_article = markdown.markdown(article_file, output_format='html5')
+    return html_article
+
+
+def render_article_page(html_article, title):
+    rendered_article_page = article_template.render(title=title, content=html_article)
+    return rendered_article_page
+
+
+def write_html_article_to_file(html_article_file, html_article_path):
+    with open(html_article_path, 'a+') as article_html:
+        article_html.seek(0)
+        article_html.truncate()
+        article_html.write(html_article_file)
+
+
+def convert_articles_to_html_pages(articles):
+    for md_article in articles:
+        html_article_path = 'site/articles/{0}.html'.format(os.path.splitext(md_article['source'])[0])
+        md_article['html'] = html_article_path
         if not os.path.exists(os.path.dirname(html_article_path)):
             os.makedirs(os.path.dirname(html_article_path))
-        logger.info(article)
-        logger.info(html_article_path)
-        title = article['title']
-        # if not (os.path.exists(html_article_path)):
-        with open('articles/{0}'.format(article['source'])) as article_file:
-            article = article_file.read()
-            html = markdown.markdown(article, output_format='html5')
-            doc = article_template.render(title=title, content=html)
-        with open(html_article_path, 'a+') as article_html:
-            article_html.seek(0)
-            article_html.truncate()
-            article_html.write(doc)
+        article_file = open_markdown_article_from_file(md_article)
+        html_article = convert_markdown_to_html(article_file)
+        rendered_article_page = render_article_page(html_article, md_article['title'])
+        write_html_article_to_file(rendered_article_page, html_article_path)
 
-def render_index():
+
+def render_index_page(articles_list):
     with open('index.html', 'w') as index:
         index.seek(0)
         index.truncate()
-        index.write(template.render(articles=articles_list))
-
-
-def convert_markdown_to_html(article_in_markdown):
-    pass
+        index.write(index_template.render(articles=articles_list))
 
 
 if __name__ == '__main__':
     articles_list = load_json_config()
-    logger.info(articles_list)
-    env = Environment(loader=FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
-    md = markdown.Markdown(extensions=['meta'])
-    env.filters['markdown'] = lambda text: Markup(md.convert(text))
-    template = env.get_template('/templates/index.html')
-    # extensions = ['markdown', 'smartypants']
+    env = create_jinja_environment()
+    index_template = env.get_template('/templates/index.html')
     article_template = env.get_template('/templates/article.html')
-    convert_articles_to_html(articles_list['articles'])
-    render_index()
+    convert_articles_to_html_pages(articles_list['articles'])
+    render_index_page(articles_list)
